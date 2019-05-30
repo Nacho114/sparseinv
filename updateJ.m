8c29f5d0f0352abe670ac2b19d2fee831d12b4dc
sidak <spssachdeva21695@gmail.com>
2019-05-30 17:29:00 +0200

    compute J_tilda by N_L correpsonding to nonzero entries of r

--- updateJ.m
+++ updateJ.m
@@ -1,13 +1,20 @@
 function [J_star] = updateJ(A, r)
     
-    [dim, ~] = size(A);
-    norm_r = norm(r)^2;
     
-    % compute decrease by taking more things
+    norm_r = norm(r)^2;
+    eps=1e-10
+    display(eps)
+    
+    L = find(r>eps)
+    J_tilda = find(sum(A(L, :)~=0, 1))'
+    %TODO: remove J from J_tilda
+    
+    % compute decrement by taking more things
+    [dim, ~] = size(J_tilda);
     pj_sqrs = zeros(dim, 1);
-    % TODO: Compute J_tilda and iterate over it instead of all the entries
-    for j = 1:dim
-
+    
+    for idx = 1:dim
+        j = J_tilda(idx)
         pj_sqrs(j) = norm_r - (r'*A(:, j))^2 / norm(A(:, j))^2;
 
     end
